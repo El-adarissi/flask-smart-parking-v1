68302db605a2f502f0c3cf1eed889baa75b3b25c
El-ADARISSI ABDELAZIZ <119929015+El-adarissi@users.noreply.github.com>
2025-03-30 10:41:50 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://default:2JVBgN6fdZEi@ep-small-cell-a48v87ie-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require')
+    # MySQL configuration
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://root:@localhost/smartparking')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.urandom(24)
+    SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key-here')
+    CORS_SUPPORTS_CREDENTIALS = True
